ef6cd01affe0b2bdbf258c8b3fbf8705782e194e
Haraki23 <seifullahelharaki@gmail.com>
2018-09-28 21:40:26 -0500

    changed thread sleep time

--- src/main/Main.java
+++ src/main/Main.java
@@ -26,7 +26,7 @@ public class Main {
 
         // For example purposes only; remove from your final project.
         try {
-            Thread.sleep(500);
+            Thread.sleep(400);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
